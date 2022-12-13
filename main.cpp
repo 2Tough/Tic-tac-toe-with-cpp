58eeb21e3bf8c3f3fc136620d8c830c45b271631
2Tough <vainferro@gmail.com>
2022-12-13 18:36:04 +0300

    Updated functions.

--- main.cpp
+++ main.cpp
@@ -11,6 +11,28 @@ int main()
 {
     char winner = ' ';
 
+    resetBoard();
+
+    while(winner == ' ' && checkFreeSpace() !=0) 
+    {
+
+    }
+    printf();
+
+    playerMove();
+    winner = checkWinner();
+    if(winner != ' ' || checkFreeSpace() == 0)
+    {
+        break;
+    }
+    computerMove();
+    winner = checkWinner();
+    if(winner != ' ' || checkFreeSpace() == 0)
+    {
+        break;
+    }
+
+    return 0;
 
 }
 
@@ -35,19 +57,117 @@ void printBoard()
     printf("\n");
     printf("");
 }
+
 int checkFreeSpace()
 {
+    int freeSpaces = 9;
 
+    for(int i = 0 ; i < 3 ; i++)
+    {
+       for(int j = 0 ; j < 3 ; j++)
+       {
+            if(board[i][j] != ' ')
+            {
+                freeSpaces--;
+            }
+       
+       }   
+    }
+    return freeSpaces;
 }
 void playerMove()
 {
+    int x;
+    int y;
 
+  do
+  {
+    printf("Enter row#(1-3): ");
+    scanf("%d" &x);
+    x--;
+    printf("Enter column#(1-3): ");
+    scanf("%d" &y);
+    y--;
+
+    if(board[x][y] != ' ') 
+    {
+        printf("Invalid move \n");
+    }
+    else
+    {
+        board[x][y] = PLAYER;
+        break;
+    }
+  } while (board[x][y] != ' ');
+  
 }
 void computerMove()
 {
+    //creates a seed based on current time
+    srand(time(0));
+    int x;
+    int y;
 
+    if(checkFreeSpace() > 0)
+    {
+        do
+        {
+            x = rand() % 3;
+            y = rand() % 3;
+        } while (board[x][y]) != ' ');
+
+        board[x][y] = COMPUTER;
+    }
+    else
+    {
+        printWinner(' ')
+    }
 }
-char checkWinner(char){
+char checkWinner(char)
+{
+    //check rows
+    for (int i = 0; i < 3; i++)
+    {
+        if(board[i][0] == board[i][1] && board[i][2])
+        {
+            return board[i][0];
+        }
+    }
+    //check columns
+    for (int i = 0; i < 3; i++)
+    {
+        if(board[0][1] == board[1][i] && board[2][i])
+        {
+            return board[0][i];
+        }
+    }
+    //check diagonals
+    if(board[0][0] == board[1][1] && board[2][2])
+    {
+        return board[0][0];
+    }
+        if(board[0][2] == board[0][2] && board[2][0])
+    {
+        return board[0][2];
+    }
 
+    return ' ';
+}
+
+void printWinner(char winner)
+{
+    if (winner == PLAYER)
+    {
+        printf("YOU WIN!");
+    }
+    else if (winner == COMPUTER)
+    {
+        printf("YOU LOSE!");
+    }
+    else{
+        printf("IT'S A TIE")
+    }
+    
+    
 }
 
