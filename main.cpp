13db3340ea518b4b351bfa0e466e39f97150315d
2Tough <vainferro@gmail.com>
2022-12-13 17:26:56 +0300

    Created board.

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+char board[3][3];
+const char PLAYER = 'X';
+const char COMPUTER = '0';
+
+int main() 
+{
+    char winner = ' ';
+
+
+}
+
+void resetBoard()
+{
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0 ; j < 3; j++)
+        {
+            board[i][j] = ' ';
+        }
+
+    }
+}
+void printBoard()
+{
+    printf(" %c | %c | %c ", board[0][0], board[0][1], board[0][2]);
+    printf("\n--|--|--\n");
+    printf(" %c | %c | %c ", board[1][0], board[1][1], board[1][2]);
+    printf("\n--|--|--\n");
+    printf(" %c | %c | %c ", board[2][0], board[2][1], board[2][2]);
+    printf("\n");
+    printf("");
+}
+int checkFreeSpace()
+{
+
+}
+void playerMove()
+{
+
+}
+void computerMove()
+{
+
+}
+char checkWinner(char){
+
+}
+
